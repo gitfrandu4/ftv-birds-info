421fd84fffe8629ef0339276d2dd15c28dca45db
Francisco J. López <frandufour@protonmail.com>
2021-05-21 22:13:30 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BirdListComponent } from './components/bird-list/bird-list.component';
+import { BirdInfoComponent } from './components/bird-info/bird-info.component';
+import { LoginComponent } from './components/login/login.component';
+import { CreateComponent } from './components/create/create.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BirdListComponent,
+    BirdInfoComponent,
+    LoginComponent,
+    CreateComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/bird-info/bird-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bird-info',
+  templateUrl: './bird-info.component.html',
+  styleUrls: ['./bird-info.component.css']
+})
+export class BirdInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
