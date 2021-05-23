d73aff602db0ff9568e7690cfd3fe28d188e10b0
Francisco J. López <frandufour@protonmail.com>
2021-05-23 21:32:58 +0100

    Añadido formulario para agregar aves + routing

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -5,8 +5,11 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
+
 export class CreateComponent implements OnInit {
 
+  public hubara_desc : string = "Los áridos y descarnados jables, malpaíses y pedregales de las islas orientales de Canarias constituyen el hábitat de la hubara canaria, una de las más singulares aves de la fauna española, cuya población —amenazada por múltiples problemas— ha disminuido alarmantemente en los últimos años. Especie perfectamente adaptada a la sequedad y a la escasez de recursos de las áreas donde habita, la hubara es un ave omnívora y muy terrestre, cuya población está siendo objeto de diferentes planes de conservación y manejo."
+  
   constructor() { }
 
   ngOnInit(): void {
