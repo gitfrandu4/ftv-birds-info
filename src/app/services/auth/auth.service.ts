d69f3df2afdb0a543455be9dc2af8a386050c235
Francisco J. López <frandufour@protonmail.com>
2021-06-03 00:08:58 +0100

    Manejando errores: login y logup

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { catchError } from "rxjs/operators";
 import { throwError } from "rxjs";
@@ -21,33 +21,70 @@ export class AuthService {
                 email: email,
                 password: password,
                 returnSecureToken: true
-            }).pipe(catchError(errorResponse => {
-                let errorMessage = "An unknown error occurred!"
+            }).pipe(catchError(this.handleError));
+            //     let errorMessage = "An unknown error occurred!"
 
-                if ( !errorResponse.error || !errorResponse.error.error ){
-                    return throwError(errorMessage);
-                }
+            //     if ( !errorResponse.error || !errorResponse.error.error ){
+            //         return throwError(errorMessage);
+            //     }
 
-                switch(errorResponse.error.error.message){
-                    case 'EMAIL_EXISTS':
-                        errorMessage = "Error - El email ya está registrado";
-                        break;
-                      default:
-                        errorMessage = "An error ocurred!";
-                        break;
-                  }
-                  return throwError(errorMessage);
-            }));
+            //     switch(errorResponse.error.error.message){
+            //         case 'EMAIL_EXISTS':
+            //             errorMessage = "Error - El email ya está registrado";
+            //             break;
+            //           default:
+            //             errorMessage = "An error ocurred!";
+            //             break;
+            //       }
+            //       return throwError(errorMessage);
+            // })
+            // );
     }
 
     // https://firebase.google.com/docs/reference/rest/auth/#section-sign-in-email-password
-    login (email: string, password: string){
+    login(email: string, password: string) {
         return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDhs2_n0VW26SI2OVGxQxoUdWZ-AvdpwUw',
-        {
-            email: email, // The email the user is siging in with
-            password: password, // The password for the account
-            returnSecureToken: true // Whether or not to return an ID and refresh token. Should always be true
-        });
+            {
+                email: email, // The email the user is siging in with
+                password: password, // The password for the account
+                returnSecureToken: true // Whether or not to return an ID and refresh token. Should always be true
+            })
+            .pipe(catchError(this.handleError));
     }
+
+    private handleError(errorResponse: HttpErrorResponse) {
+        let errorMessage = "An unknown error occurred!"
+
+        if (!errorResponse.error || !errorResponse.error.error) {
+            return throwError(errorMessage);
+        }
+
+        switch (errorResponse.error.error.message) {
+            // Common error codes - logup
+            case 'EMAIL_EXISTS':
+                errorMessage = "Error - The email address is already in use by another account.";
+                break;
+            case 'OPERATION_NOT_ALLOWED':
+                errorMessage = "Error - Password sign-in is disabled for this project.";
+                break;
+            case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+                errorMessage = "Error - We have blocked all requests from this device due to unusual activity. Try again later.";
+                break;
+            // Common error codes - login
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'Error - There is no user record corresponding to this identifier. The user may have been deleted.';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'Error - The password is invalid or the user does not have a password.';
+                break;
+            case 'USER_DISABLED':
+                errorMessage = 'Error - The user account has been disabled by an administrator.';
+                break;
+            default:
+                errorMessage = "An unknown error ocurred!";
+                break;
+        }
+        return throwError(errorMessage);
+}
 
 }
